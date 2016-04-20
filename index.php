d3e9fb80ba8ec95162925befa1b7ad82ab95216a
jestertaguibao <taguibaojester@gmail.com>
2016-04-20 17:50:19 +0800

    test

--- index.php
+++ index.php
@@ -2,9 +2,21 @@
 <html>
 <head>
 	<title></title>
+<!-- CSS -->
 <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<!-- CSS -->
 </head>
-<body>
-<h1>Test</h1>
-</body>
+	<body>
+	<h1>CRUD</h1>
+
+		<div class="btn-group" role="group" aria-label="...">
+		  <button type="button" class="btn btn-default">Left</button>
+		  <button type="button" class="btn btn-default">Middle</button>
+		  <button type="button" class="btn btn-default">Right</button>
+		</div>
+
+	</body>
+<!-- SCRIPT -->
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<!-- SCRIPT -->
 </html>
