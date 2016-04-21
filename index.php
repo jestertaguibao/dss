c8233498d9ea8e03d4395c5bc5fec982991b80d4
jestertaguibao <taguibaojester@gmail.com>
2016-04-21 17:26:31 +0800

    added data

--- index.php
+++ index.php
@@ -37,7 +37,7 @@
 		  </thead>
 		  <tbody>
 		    
-		    <?php include('common/table.php'); ?>
+		    <?php include('tables/table_1.php'); ?>
 		    
 		  </tbody>
 		</table>
@@ -50,29 +50,9 @@
 			<div class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel">
 			  <div class="modal-dialog modal-lg">
 			    <div class="modal-content">
-				    <!-- fields -->
-					      <form>
-						  <div class="form-group">
-						    <label for="exampleInputEmail1">Email address</label>
-						    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-						  </div>
-						  <div class="form-group">
-						    <label for="exampleInputPassword1">Password</label>
-						    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-						  </div>
-						  <div class="form-group">
-						    <label for="exampleInputFile">File input</label>
-						    <input type="file" id="exampleInputFile">
-						    <p class="help-block">Example block-level help text here.</p>
-						  </div>
-						  <div class="checkbox">
-						    <label>
-						      <input type="checkbox"> Check me out
-						    </label>
-						  </div>
-						  <button type="submit" class="btn btn-default">Submit</button>
-						</form>
-					<!-- fields -->
+				    <!-- forms -->
+						<?php include('forms/form_1.php'); ?>
+					<!-- forms -->
 
 			    </div>
 			  </div>
