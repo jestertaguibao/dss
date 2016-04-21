73789e7e9018175025ab22fe194d24ac68a51a7a
jestertaguibao <taguibaojester@gmail.com>
2016-04-21 14:04:23 +0800

    added data

--- index.php
+++ index.php
@@ -7,13 +7,41 @@
 <!-- CSS -->
 </head>
 	<body>
-	<h1>CRUD</h1>
 
-		<div class="btn-group" role="group" aria-label="...">
-		  <button type="button" class="btn btn-default">Left</button>
-		  <button type="button" class="btn btn-default">Middle</button>
-		  <button type="button" class="btn btn-default">Right</button>
-		</div>
+		<!-- navbar -->
+		<nav class="navbar navbar-default">
+		  <div class="container-fluid">
+		    <div class="navbar-header">
+		      <a class="navbar-brand" href="#">
+		       test
+		      </a>
+		    </div>
+		  </div>
+		</nav>
+		<!-- navbar -->
+	<center>
+	<h1>CRUD</h1>
+	</center>
+		<?php include('common/db_connect.php'); ?>
+		<!-- table -->
+		<table class="table table-hover">
+		  <thead>
+		    <tr>
+		      <th>ID</th>
+		      <th>Email</th>
+		      <th>Password</th>
+		      <th>Name</th>
+		      <th>Address</th>
+		    </tr>
+		  </thead>
+		  <tbody>
+		    
+		    
+
+		    <?php include('table.php'); ?>
+		    
+		  </tbody>
+		</table>
 
 	</body>
 <!-- SCRIPT -->
---
+++ table.php
@@ -0,0 +1,25 @@
+
+			
+
+<?php 
+	$sql = "SELECT * FROM user";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+?>			
+			<tr>
+		      <th scope="row"><?php echo $row["id"]; ?></th>
+		      <td><?php echo $row["email"]; ?></td>
+		      <td><?php echo $row["password"]; ?></td>
+		      <td><?php echo $row["name"]; ?></td>
+		      <td><?php echo $row["address"]; ?></td>
+		    </tr>
+<?php
+	    }
+	} else {
+	    echo "0 results";
+	}
+	$conn->close();
+?>
