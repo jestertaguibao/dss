652a3473d3159b1cba4eecae7fc38829e2b4b049
jestertaguibao <taguibaojester@gmail.com>
2016-04-21 17:54:48 +0800

    added fields

--- forms/form_1.php
+++ forms/form_1.php
@@ -1,21 +1,26 @@
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+<div class="alert alert-info" role="alert"><h3>Add new data</h3></div>
+<center>  
+<div class="row">
+  <div class="col-md-8 col-md-offset-2">
+    <form>
+      <div class="form-group">
+        
+        <label>Email address</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
+        <br>
+        <label>Password</label>
+        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+        <br>
+        <label>Name</label>
+        <input type="text" class="form-control" placeholder="Text input">
+        <br>
+        <label>Address</label>
+        <textarea class="form-control" rows="3"></textarea>
+      
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+    <br>
   </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-  </div>
-  <div class="form-group">
-    <label for="exampleInputFile">File input</label>
-    <input type="file" id="exampleInputFile">
-    <p class="help-block">Example block-level help text here.</p>
-  </div>
-  <div class="checkbox">
-    <label>
-      <input type="checkbox"> Check me out
-    </label>
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
+</div>
+</center>
